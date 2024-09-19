38ef29a467de9318f5a9055485e461bd68ab7324
Najiib <maxamednajiib10@hotmail.com>
2024-09-19 11:41:46 -0600

    add an operate function

--- calculator.js
+++ calculator.js
@@ -10,3 +10,17 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+
+let firstNum; let secNum; let operator;
+
+function operate(num_1, operator, num_2){
+    if (operator === "+"){
+        return add(num_1, num_2);
+    } else if (operator === "-"){
+        return subtract(num_1, num_2);
+    } else if (operator === "*"){
+        return multiply(num_1, num_2);
+    } else if (operator === "/"){
+        return divide(num_1, num_2);
+    }
+}
