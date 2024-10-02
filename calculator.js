09024b809d56c31f58fe194fb98f38bd5a4013cd
Najiib <maxamednajiib10@hotmail.com>
2024-10-02 11:33:37 -0600

    calculator accepts multiple digit numbers

--- calculator.js
+++ calculator.js
@@ -10,9 +10,14 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
-
-let firstNum; let secNum; let operator;
-
+function isNumber(number){
+    let numbers = "0123456789";
+    return numbers.includes(number);
+}
+function populateDisplay(num){
+    display = document.querySelector(".display");
+    display.textContent = num;
+}
 function operate(num_1, operator, num_2){
     if (operator === "+"){
         return add(num_1, num_2);
@@ -23,4 +28,61 @@ function operate(num_1, operator, num_2){
     } else if (operator === "/"){
         return divide(num_1, num_2);
     }
+}
+function clearBtn(){
+    display.textContent = 0;
+    firstNum = undefined;
+    secNum = undefined;
+    operator = undefined;
+
+}
+function btnClick(e){
+    num = e.target.textContent;
+    if (num === "Ac"){
+        return clearBtn();
+    }
+    if (isNumber(num)){
+        if(firstNum === undefined){
+            firstNum = num;
+            populateDisplay(firstNum);
+            
+        }else if (secNum === undefined && operator === undefined){
+            if (typeof firstNum === "number"){
+                firstNum = num;
+            } else{
+                firstNum = firstNum.concat(num);
+            }
+            populateDisplay(firstNum);
+
+        }else if(secNum ===undefined){
+            secNum = num;
+            populateDisplay(secNum);
+
+        }else if(operator != undefined){
+            secNum = secNum.concat(num);
+            populateDisplay(secNum);
+        }
+        // populateDisplay(num);
+    }
+    if("+-*/".includes(num)){
+        operator = num;
+    }
+
+    if (num == "="){
+        if (firstNum != undefined && secNum !==undefined && operator !== undefined){
+            console.log(Number(firstNum));
+            console.log(Number(secNum))
+            console.log(operator)
+            let calculatedVal = operate(Number(firstNum), operator, Number(secNum));
+            firstNum = calculatedVal;
+            operator = undefined;
+            secNum = undefined;
+            populateDisplay(calculatedVal);
+        }
+    }
+}
+let firstNum, secNum, operator, display;
+const btns = document.querySelectorAll("button");
+for(let i=0; i < btns.length; i++){
+    btns[i].addEventListener("click", btnClick)
 }
